3ba39d57cc367bf52dfb823b5a83773456fd95f1
gabriel velo <gabriel.velo@gmail.com>
2024-09-19 00:45:46 -0300

    refactor: simplify the code for generating character groups

--- src/koch/groups.test.ts
+++ src/koch/groups.test.ts
@@ -1,42 +1,51 @@
-import { buildGroups, MIN_CHAR_REPETITIONS } from "./groups";
+import { buildGroups, getLessonChars, MIN_CHAR_REPETITIONS } from "./groups";
 
-describe('test groups', () => {
-    test('test distributions on evenly sized groups of 2 characters', () => {
-        const groups = buildGroups(1, 'K', 10, 4);
-        const charCount = countCharRepetitions(groups);
-        expect(charCount).toEqual({ 'K': 25, 'M': 25 });
-        expect(1).toBe(1);
-    });
+describe("test groups", () => {
+  test("test distributions on evenly sized groups of 2 characters", () => {
+    const groups = buildGroups("K", ["M"], 4);
+    const charCount = countCharRepetitions(groups);
+    expect(charCount).toEqual({ K: 10, M: 10 });
+    expect(1).toBe(1);
+  });
 
-    test('test distributions on odd groups  of 2 chars', () => {
-        const groups = buildGroups(1, 'K', 11, 4);
-        const charCount = countCharRepetitions(groups);
-        expect(charCount).toEqual({ 'K': 28, 'M': 27 });
-        expect(1).toBe(1);
-    });
+  test("test distributions on odd groups  of 2 chars", () => {
+    const groups = buildGroups("K", ["M"], 5);
+    const charCount = countCharRepetitions(groups);
+    expect(charCount).toEqual({ K: 13, M: 12 });
+    expect(1).toBe(1);
+  });
 
-    test('test min repetition', () => {
-        const groups = buildGroups(11, 'L', 12, 400);
-        const charCount = countCharRepetitions(groups);
-        for (const repetitions of Object.values(charCount)) {
-            expect(repetitions).toBeGreaterThanOrEqual(MIN_CHAR_REPETITIONS);
-        }
-    });
+  test("test min repetition", () => {
+    const groups = buildGroups("A", ["B", "C", "D", "E"], 2);
+    const charCount = countCharRepetitions(groups);
+    console.log(charCount);
+    expect(Object.keys(charCount).length).toEqual(2);
+    for (const repetitions of Object.values(charCount)) {
+      expect(repetitions).toBeGreaterThanOrEqual(MIN_CHAR_REPETITIONS);
+    }
+  });
+
+  test("test lesson chars", () => {
+    for (let i = 1; i < 41; i++) {
+      console.log(getLessonChars(i));
+    }
+  });
 });
 
+function countCharRepetitions(groups: Array<string>): {
+  [char: string]: number;
+} {
+  const chars = groups.join("");
 
-function countCharRepetitions(groups: Array<string>): { [char: string]: number } {
-    const chars = groups.join('');
+  const charCount: { [char: string]: number } = {};
 
-    const charCount: { [char: string]: number } = {};
-
-    for (const char of chars) {
-        if (charCount[char]) {
-            charCount[char]++;
-        } else {
-            charCount[char] = 1;
-        }
+  for (const char of chars) {
+    if (charCount[char]) {
+      charCount[char]++;
+    } else {
+      charCount[char] = 1;
     }
+  }
 
-    return charCount;
+  return charCount;
 }
--- src/koch/groups.ts
+++ src/koch/groups.ts
@@ -45,7 +45,7 @@ const MORSE_SYMBOLS: string[] = [
 export const GROUP_LEN: number = 5;
 export const MIN_CHAR_REPETITIONS = 5;
 
-class GroupCharSet extends Array<string> {
+class LessonChars extends Array<string> {
   addCharacter(character: string, repetitions: number): void {
     for (let i = 0; i < repetitions; i++) {
       this.push(character);
@@ -53,67 +53,48 @@ class GroupCharSet extends Array<string> {
   }
 }
 
-export class Groups extends Array<string> {
-  constructor(
-    lessonLevel: number,
-    mainCharacter: string,
-    groupCount: number,
-    maxSecondaryCharacters: number
-  ) {
-    super();
+export function buildGroups(
+  mainChar: string,
+  secondaryChars: string[],
+  groupCount: number
+): string[] {
+  const lessonCharCount = groupCount * GROUP_LEN;
+  const secondaryLessonCharCount = Math.floor(lessonCharCount / 2);
+  const mainLessonCharCount = lessonCharCount - secondaryLessonCharCount;
 
-    let availableSecondaryCharacters = MORSE_SYMBOLS.slice(
-      0,
-      lessonLevel + 1
-    ).filter((char) => char !== mainCharacter);
+  const lessonChars = new LessonChars();
+  lessonChars.addCharacter(mainChar, mainLessonCharCount);
 
-    const totalCharCountInLesson = groupCount * GROUP_LEN;
-    const secondaryCharCount = Math.floor(totalCharCountInLesson / 2);
-    const mainCharacteCount = totalCharCountInLesson - secondaryCharCount;
+  let secRepetitionCount = secondaryLessonCharCount / secondaryChars.length;
+  let secRepetitionRemainder = secondaryLessonCharCount % secondaryChars.length;
+  let secondaryCharsToAdd = [...secondaryChars];
 
-    const lessonCharacters = new GroupCharSet();
-
-    lessonCharacters.addCharacter(mainCharacter, mainCharacteCount);
-
-    let secondaryCharsToAddCount =
-      availableSecondaryCharacters.length > maxSecondaryCharacters
-        ? maxSecondaryCharacters
-        : availableSecondaryCharacters.length;
-
-    let secondaryRepetitions = Math.floor(
-      secondaryCharCount / secondaryCharsToAddCount
-    );
-    
-    let secondaryRemainder = secondaryCharCount % secondaryCharsToAddCount;
-
-    if (secondaryRepetitions < MIN_CHAR_REPETITIONS) {
-      secondaryCharsToAddCount = secondaryCharCount / MIN_CHAR_REPETITIONS;
-      secondaryRepetitions = MIN_CHAR_REPETITIONS;
-      secondaryRemainder = secondaryCharsToAddCount % MIN_CHAR_REPETITIONS;
-    }
-
-    if (availableSecondaryCharacters.length > secondaryCharsToAddCount) {
-      availableSecondaryCharacters = shuffleArray(
-        availableSecondaryCharacters
-      ).slice(0, secondaryCharsToAddCount);
-    }
-
-    const secondayDistribution = buildSecDistribution(
-      availableSecondaryCharacters,
-      secondaryRepetitions,
-      secondaryRemainder
-    );
-
-    for (const dist of secondayDistribution) {
-      lessonCharacters.addCharacter(dist.char, dist.repetitions);
-    }
-
-    const shuffledChars = shuffleArray(lessonCharacters);
-
-    for (let i = 0; i < shuffledChars.length; i += GROUP_LEN) {
-      this.push(shuffledChars.slice(i, i + GROUP_LEN).join(""));
-    }
+  if (secRepetitionCount < MIN_CHAR_REPETITIONS) {
+    secRepetitionCount = MIN_CHAR_REPETITIONS;
+    const secCharCount = secondaryLessonCharCount / MIN_CHAR_REPETITIONS;
+    secondaryCharsToAdd = shuffleArray(secondaryChars).slice(0, secCharCount);
+    secRepetitionRemainder = secondaryLessonCharCount % secCharCount;
   }
+
+  const secondayDistribution = buildSecDistribution(
+    secondaryCharsToAdd,
+    secRepetitionCount,
+    secRepetitionRemainder
+  );
+
+  for (const dist of secondayDistribution) {
+    lessonChars.addCharacter(dist.char, dist.repetitions);
+  }
+
+  const shuffledChars = shuffleArray(lessonChars);
+
+  let groups: string[] = [];
+
+  for (let i = 0; i < shuffledChars.length; i += GROUP_LEN) {
+    groups.push(shuffledChars.slice(i, i + GROUP_LEN).join(""));
+  }
+
+  return groups;
 }
 
 function buildSecDistribution(
@@ -121,7 +102,6 @@ function buildSecDistribution(
   repetitions: number,
   remainder: number
 ): Array<{ char: string; repetitions: number }> {
-
   const dist = new Array<{ char: string; repetitions: number }>();
 
   for (const char of secondayChars) {
@@ -135,15 +115,6 @@ function buildSecDistribution(
   return dist;
 }
 
-export function buildGroups(
-  lesson: number,
-  mainChar: string,
-  numOfGroups: number,
-  maxSecCharacters: number
-): Groups {
-  return new Groups(lesson, mainChar, numOfGroups, maxSecCharacters);
-}
-
 function shuffleArray<T>(array: T[]): T[] {
   const shuffledArray = array.slice();
   for (let i = shuffledArray.length - 1; i > 0; i--) {
@@ -151,4 +122,17 @@ function shuffleArray<T>(array: T[]): T[] {
     [shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]];
   }
   return shuffledArray;
+}
+
+export interface Lesson {
+  mainChar: string;
+  secondaryChars: string[];
+}
+
+export function getLessonChars(lessonNumber: number): Lesson {
+  let lesson: Lesson = {
+    mainChar: MORSE_SYMBOLS[lessonNumber],
+    secondaryChars: MORSE_SYMBOLS.slice(0, lessonNumber),
+  };
+  return lesson;
 }
