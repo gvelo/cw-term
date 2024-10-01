210d6bd2ef7b5ea64a0bd4addb64a2986ad0f664
gabriel velo <gabriel.velo@gmail.com>
2024-10-01 01:11:58 -0300

    feat: add global help

--- src/index.ts
+++ src/index.ts
@@ -4,29 +4,5 @@ let termDiv = document.getElementById("terminal");
 
 const term = new Terminal(termDiv!);
 
-writeWellcome(term);
-
 const root = new RootApp(term);
 root.start();
-
-
-// term.onKey((e: { key: string, domEvent: KeyboardEvent }) => {
-//     term.write(e.key);
-//     let cw = new jscw();
-//     cw.setText(groupsStr);
-//     cw.setEff(6);
-//     cw.setWpm(20);
-//     cw.play();
-// })
-
-function writeWellcome(term: Terminal) {
-    term.writeln("wellcome to cw-terminal");
-}
-
-
-
-
-
-
-
-
--- src/root.ts
+++ src/root.ts
@@ -18,6 +18,7 @@ import { Terminal } from "./Terminal";
 import { TxCommand } from "./txcmd";
 import { StatusBar } from "./statusbar";
 import { KochCommand } from "./koch/cmd";
+import { theme } from "./theme";
 
 export class RootApp {
   private terminal: Terminal;
@@ -37,6 +38,7 @@ export class RootApp {
   }
 
   async start() {
+    this.showWellcome();
     while (true) {
       const prompt = "cw-console: ";
       const line = await this.terminal.readLine(prompt);
@@ -53,7 +55,7 @@ export class RootApp {
       const cmd = argv[0];
       switch (cmd) {
         case "help":
-          this.printAppHelp();
+          this.showUsage();
           break;
         case "koch":
           await this.kochCmd.exec(argv);
@@ -62,7 +64,12 @@ export class RootApp {
           this.statusBar.setCmd("tx");
           await this.txCmd.exec(argv);
           break;
-
+        case "callsign":
+          this.terminal.writeln("not yet implemented");
+          break;
+        case "qso":
+          this.terminal.writeln("not yet implemented");
+          break;
         default:
           this.terminal.writeln(`command not found: ${cmd}`);
           break;
@@ -103,7 +110,55 @@ export class RootApp {
     return argv;
   }
 
-  printAppHelp() {
-    this.terminal.writeln("help");
+  private showWellcome(): void {
+    this.terminal.writeln(`
+                                            _       
+  ____ _ _ _     ____ ___  ____   ___  ___ | | ____ 
+ / ___) | | |   / ___) _ \\|  _ \\ /___)/ _ \\| |/ _  )
+( (___| | | |  ( (__| |_| | | | |___ | |_| | ( (/ / 
+ \\____)\\____|   \\____)___/|_| |_(___/ \\___/|_|\\____)
+
+type 'help' if you are new!                    (v0.0.1)  
+
+  `);
+  }
+
+  showUsage() {
+    this.terminal.writeln(
+      theme.info(`
+
+cw-console is a simple Morse code training program designed to help you improve
+your CW (Continuous Wave) abilities. The following subcommands provide various 
+methods to practice and enhance your skills:
+
+  koch:
+    Train Morse code using the Koch method, which introduces characters gradually, 
+    starting with just two characters and adding more as you become proficient. 
+    This method focuses on listening at full speed from the start, helping you develop 
+    accuracy and speed simultaneously.
+    Type 'koch --help' for more details.
+
+  callsign:
+    Practice receiving and decoding callsigns, a key skill for amateur radio operators. 
+    This exercise helps you become proficient in identifying callsigns quickly and 
+    accurately.
+    Type 'callsign --help' for more details.
+
+  qso:
+    Train using a standard QSO (conversation) format, simulating real-world Morse 
+    code exchanges. This mode helps you get comfortable with the structure and flow 
+    of typical CW QSOs.
+    Type 'qso --help' for more details.
+
+  tx:
+    Transmit arbitrary text in Morse code. This mode allows you to practice sending 
+    text at your chosen speed, tone, and settings, which can be customized to fit 
+    your preferences.
+    Type 'tx --help' for more details.
+
+For more information on any specific subcommand, use the '--help' flag after the subcommand name.
+
+      `)
+    );
   }
 }
